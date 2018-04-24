3ffbe64968001e9d161cb87b1b2a251191c3742a
code TT <372867575@qq.com>
2018-04-24 15:16:50 +0800

    Initial Commit

---
+++ TTMarqueeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTMarqueeDemo
+//
+//  Created by iOS开发 on 2018/4/24.
+//  Copyright © 2018年 燃烧的蔬菜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTMarqueeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTMarqueeDemo
+//
+//  Created by iOS开发 on 2018/4/24.
+//  Copyright © 2018年 燃烧的蔬菜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
